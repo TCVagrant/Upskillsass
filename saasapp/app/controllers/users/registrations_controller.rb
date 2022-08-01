0636dc55c3f37cdf2a0cda1dea306607a573a961
Terry Chuong <alchemytext@gmail.com>
2022-08-01 21:12:04 +0000

    Updated flash message styles and js

--- saasapp/app/controllers/users/registrations_controller.rb
+++ saasapp/app/controllers/users/registrations_controller.rb
@@ -1,4 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  # Extend default Devise gem behavior so that users signing up with the PPro 
+  # account save with a special stripe subscription function
+  # Otherwise devise signs up user as usual
   def create
     super do |resource|
       if params[:plan]
